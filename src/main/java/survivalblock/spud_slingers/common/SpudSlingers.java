dbd56ceda3f2ff4c8c75ae6a124538fc1c59222f
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-06-16 20:20:42 -0700

    Summary (required)

---
+++ src/main/java/survivalblock/spud_slingers/common/SpudSlingers.java
@@ -0,0 +1,16 @@
+package survivalblock.spud_slingers.common;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpudSlingers implements ModInitializer {
+	public static final String MOD_ID = "spud_slingers";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/survivalblock/spud_slingers/common/datagen/SpudSlingersDataGenerator.java
@@ -0,0 +1,11 @@
+package survivalblock.spud_slingers.common.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpudSlingersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
